e74c10e4ef13a783577d604593a029f36d24d76f
Justin Buchanan <justbuchanan@gmail.com>
2013-09-08 02:02:46 -0400

    moved KeyPath implementation to .cpp file

--- KeyPath.hpp
+++ KeyPath.hpp
@@ -9,45 +9,17 @@
 namespace CConf {
 	class KeyPath {
 	public:
-		KeyPath(const char *str) {
-			std::string s = str;
-			_loadFromString(s);
-		}
-
-		KeyPath(std::string &str) {
-			_loadFromString(str);
-		}
-
+		KeyPath(const char *str);
+		KeyPath(std::string &str);
 		KeyPath(KeyPath &other) : _components(other._components) {}
 
-		bool operator==(KeyPath &other) {
-			//	FIXME
-
-			return true;
-		}
-
-		std::string description() {
-			std::stringstream ss;
-			ss << "<";
-			for ( int i = 0; i < _components.size(); i++ ) {
-				if ( i != 0 ) ss << ".";
-				ss << _components[i];
-			}
-			ss << ">";
-
-			return ss.str();
-		}
+		bool operator==(KeyPath &other);
+		std::string &operator[](unsigned int i);
+		std::string description();
 
 
 	protected:
-		void _loadFromString(std::string &str) {
-			_components.clear();
-			boost::char_separator<char> sep(".");
-			boost::tokenizer< boost::char_separator<char> > tokens(str, sep);
-			BOOST_FOREACH(const std::string& t, tokens) {
-				_components.push_back(t);
-			}
-		}
+		void _loadFromString(std::string &str);
 
 
 	private:
