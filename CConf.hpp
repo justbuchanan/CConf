48f5b62e652946f6c97c319db9078bb77270484e
Justin Buchanan <justbuchanan@gmail.com>
2014-08-19 21:15:22 -0400

    removed all old stuff - starting over

--- CConf.hpp
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-#include "Context.hpp"
-#include "Tree.hpp"
-#include "Item.hpp"
-#include "KeyPath.hpp"
--- KeyPathObserver.cpp
+++
@@ -1,5 +0,0 @@
-
-#include "KeyPathObserver.hpp"
-
-
-CConf::KeyPathObserver(CConf::Context &ctxt, CConf::KeyPath &keyPath) : _context(ctxt), _keyPath(keyPath) {}
