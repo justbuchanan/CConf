af7ca3a8fd96714d2e6c83823f6135ef7b0b512a
Justin Buchanan <justbuchanan@gmail.com>
2014-11-16 01:12:12 -0500

    did some xml test stuff in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <fstream>
 
 #include <QApplication>
 #include <QMainWindow>
+
+#include <json/json.h>
 
 using namespace std;
 
 
 int main(int argc, char **argv) {
+
+    ifstream doc("example.json");
+
+    Json::Value root;
+    Json::Reader reader;
+
+
+    // parse(const std::string& document, Value& root, bool collectComments = true);
+
+
+
+    bool success = reader.parse(doc, root, false);
+    if (!success) {
+        cerr << "Error parsing json: " << reader.getFormattedErrorMessages();
+        return EXIT_FAILURE;
+    }
+
+    cout << "Value: " << endl << root;
+
     return 0;
 }
