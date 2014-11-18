a6a2f1a95916dd3dd37b4e008ff46d56430bcb36
Justin Buchanan <justbuchanan@gmail.com>
2014-11-17 21:12:48 -0500

    more stuff...

--- src/ConfigContext.hpp
+++ src/ConfigContext.hpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
+#include <algorithm>
 #include <QFileSystemWatcher>
 #include <QAbstractItemModel>
 
@@ -26,7 +27,7 @@ private:
 
 
 
-class ConfigContext : public QObject {
+class ConfigContext : public QAbstractItemModel {
     Q_OBJECT
 
 public:
@@ -42,7 +43,6 @@ public:
         if (containsFile(path)) {
             throw "this config file is already loaded";
         }
-
 
         auto tree = readFile(path);
         if (!tree) {
@@ -90,7 +90,44 @@ public:
     }
 
 
-//  ITEM MODEL STUFF
+    #pragma mark - QAbstractItemModel Stuff -
+
+    //  see the article on Qt's website for more info on how to subclass QAbstractItemModel
+    //  http://qt-project.org/doc/qt-4.8/itemviews-simpletreemodel.html
+
+
+    QModelIndex index(int row, int column, const QModelIndex &parent) const {
+        //  return invalid model index if the request was invalid
+        if (!hasIndex(row, column, parent)) return QModelIndex();
+
+        Node *parentNode = parent.isValid() ? static_cast<Node*>(parent.internalPointer()) : _rootNode;
+
+        Node *childNode = parentNode->_childAtIndex(row);
+        return childNode != nullptr ? createIndex(row, column, childNode) : QModelIndex();
+    }
+
+    QModelIndex parent(const QModelIndex &child) const {
+        if (!child.isValid()) return QModelIndex();
+
+        Node *childNode = static_cast<Node*>(child.internalPointer());
+        Node *parentNode = childNode->parent();
+
+        if (parentNode == _rootNode) return QModelIndex();
+
+        return createIndex(parentNode->row(), 0, parentNode);
+    }
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const {
+        return 0;   //  FIXME:
+    }
+
+    int columnCount(const QModelIndex &parent = QModelIndex()) const {
+        return 0;   //  FIXME: 
+    }
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const {
+        return QVariant();  //  FIXME
+    }
 
 
 
@@ -98,8 +135,12 @@ protected:
 
     class Node {
     public:
-        Node() {
+        Node(Node *parent = nullptr) {
+            _parent = parent;
+        }
 
+        ~Node() {
+            if (_parent) delete _parent;
         }
 
         bool isMapNode() const {
@@ -122,6 +163,56 @@ protected:
             }
         }
 
+        Node *&operator[](string key) {
+            if (isMapNode()) {
+                return _mappedSubnodes[key];
+            } else {
+                throw invalid_argument("Attempt to subscript into non-map Node");
+            }
+        }
+
+
+        //  Methods for QAbstractItemModel
+        ////////////////////////////////////////////////////////////////////////////////////
+
+        Node *_childAtIndex(int index) {
+            if (isMapNode()) {
+                throw invalid_argument("Justin has yet to implement this");
+                auto itr = _mappedSubnodes.begin();
+                return itr->second;
+            } else if (isArrayNode()) {
+                return (*this)[index];
+            } else {
+                throw invalid_argument("Attempt to get child of leaf Node");
+                return nullptr;
+            }
+        }
+
+        int row() {
+            if (_parent) {
+                return _parent->indexOfSubnode(this);
+            } else {
+                return 0;
+            }
+        }
+
+        int indexOfSubnode(const Node *child) const {
+            if (isArrayNode()) {
+                auto itr = std::find(_indexedSubnodes.begin(), _indexedSubnodes.end(), child);
+                if (itr == _indexedSubnodes.end()) return -1;
+
+                return itr - _indexedSubnodes.begin();
+            } else if (isMapNode()) {
+                throw invalid_argument("Justin needs to implement this too");
+            } else {
+                throw invalid_argument("Attempt to get ask leaf node about its subnodes");
+            }
+        }
+
+        Node *parent() {
+            return _parent;
+        }
+
 
     protected:
         friend class ConfigContext;
@@ -138,8 +229,37 @@ protected:
         //  if this is a parent node
         map<string, Node*> _mappedSubnodes;
         vector<Node*> _indexedSubnodes;
+        Node *_parent;
     };
 
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+
+    class PathNode : public Node {
+    public:
+        PathNode(PathNode *parent = nullptr) : Node(parent) {}
+    };
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+
+    class ScopedValueNode : public Node {
+    public:
+        ScopedValueNode(PathNode *parent) : Node(parent) {}
+
+        const bool isDefaultScope() const {
+            return _scope.size() == 0;
+        }
+
+        const vector<string> &scope() const {
+            return _scope;
+        }
+
+    private:
+        vector<string> _scope;
+    };
 
 
     void insertTree(const Json::Value &tree) {
@@ -151,7 +271,7 @@ protected:
 private:
     //  higher index = higher precedence when cascading values
     vector<pair<string, shared_ptr<Json::Value>>> _configTrees;
-    Node _combined;
+    PathNode *_rootNode;
     QFileSystemWatcher _fsWatcher;
 };
 
